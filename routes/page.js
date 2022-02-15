b1362262a074a2b4af41727e8f0fa9669ea65966
twoonezero <dnjsdudtm98@naver.com>
2022-02-16 00:18:41 +0900

    Setting User-Model

---
+++ routes/page.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  //res.locals -> public 에 정보 넘겨 줌
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', {
+    title: '내 정보 - NodeBird',
+  });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', {
+    title: '회원가입 - NodeBird',
+  });
+});
+
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+module.exports = router;
